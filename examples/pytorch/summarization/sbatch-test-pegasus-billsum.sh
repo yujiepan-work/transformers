265e50500d9e219e2ceb64c9d3e3801f7fc7b02b
Chua, Vui Seng <vui.seng.chua@intel.com>
2021-12-16 17:01:01 -0800

    rename sbatch-eval-pegasus-billsum.sh -> sbatch-test-pegasus-billsum.sh

--- examples/pytorch/summarization/sbatch-test-pegasus-billsum.sh
+++ examples/pytorch/summarization/sbatch-test-pegasus-billsum.sh
@@ -7,13 +7,13 @@ conda activate dgx4-pegasus-hf4p13
 WORKDIR=/data1/vchua/dgx4-pegasus-hf4p13/transformers/examples/pytorch/summarization
 
 export CUDA_VISIBLE_DEVICES=0
-export WANDB_PROJECT="pegasus-eval"
+export WANDB_PROJECT="pegasus-test"
 export WANDB_DISABLED=false # Enable wandb
 export WANDB_WATCH=false # Disable gradient serialization to wandb
 
 DT=$(date +%F_%H-%M)
 RUNID=pegasus-billsum-${DT}
-OUTDIR=/data1/vchua/runs-pegasus-hf4p13/pegasus-eval/${RUNID}
+OUTDIR=/data1/vchua/runs-pegasus-hf4p13/pegasus-test/${RUNID}
 mkdir -p $OUTDIR
 
 cd $WORKDIR
@@ -23,7 +23,7 @@ python run_summarization.py \
     --dataset_name billsum \
     --max_source_length 1024 \
     --max_target_length 256 \
-    --do_eval \
+    --do_predict \
     --per_device_eval_batch_size 8 \
     --predict_with_generate \
     --num_beams 8 \
