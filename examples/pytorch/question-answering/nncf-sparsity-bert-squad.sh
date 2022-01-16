8402551741fd29a335d88663416adaac2bb9425b
Chua, Vui Seng <vui.seng.chua@intel.com>
2022-01-16 12:35:18 -0800

    add dryrun mode to nncf-sparsity-bert-squad.sh

--- examples/pytorch/question-answering/nncf-sparsity-bert-squad.sh
+++ examples/pytorch/question-answering/nncf-sparsity-bert-squad.sh
@@ -7,12 +7,12 @@ export WANDB_USERNAME=vchua
 export WANDB_API_KEY=f8a95080288950342f1695008cd8256adc3b0778
 
 # ---------------------------------------------------------------------------------------------
-export WANDB_PROJECT="tld-poc ($(hostname))"
+export WANDB_PROJECT="pruneofa-tl ($(hostname))"
 export CUDA_VISIBLE_DEVICES=3
-NEPOCH=10
-SPARSITY=50pc
-RUNID=run01-bert-squad-cropped-magsparse-${SPARSITY}-${NEPOCH}eph
-OUTROOT=/data1/vchua/tld-poc-$(hostname)
+NEPOCH=5
+SPARSITY=90pc
+RUNID=run02-bert-squad-pruneofa-${SPARSITY}-qat-customkd-lt-${NEPOCH}eph
+OUTROOT=/data1/vchua/tld-poc-$(hostname)/pruneofa-tl/
 WORKDIR=/home/vchua/tld-poc/transformers/examples/pytorch/question-answering
 
 CONDAROOT=/data1/vchua
@@ -25,21 +25,24 @@ cd $WORKDIR
 
 cmd="
 python run_qa.py \
-    --model_name_or_path /data1/vchua/tld-poc/bert-base-squadv1-local-hybrid-compiled \
-    --optimize_model_before_eval \
-    --optimized_checkpoint /data1/vchua/tld-poc/bert-base-squadv1-local-hybrid-filled-lt-compiled \
+    --model_name_or_path /data1/vchua/tld-poc-csr-dgx1-03/pruneofa-tl/run01-bert-squad-pruneofa-90pc-8eph/checkpoint-56750 \
     --dataset_name squad \
     --do_eval \
     --do_train \
     --evaluation_strategy steps \
     --eval_steps 250 \
     --learning_rate 3e-5 \
+    --teacher bert-large-uncased-whole-word-masking-finetuned-squad \
+    --teacher_ratio 0.9 \
+    --lr_scheduler_type cosine_with_restarts \
+    --warmup_ratio 0.25 \
+    --cosine_cycles 1 \
     --num_train_epochs $NEPOCH \
     --per_device_eval_batch_size 128 \
     --per_device_train_batch_size 16 \
     --max_seq_length 384 \
     --doc_stride 128 \
-    --save_steps 2500 \
+    --save_steps 250 \
     --nncf_config nncf_bert_squad_sparsity.json \
     --logging_steps 1 \
     --overwrite_output_dir \
@@ -53,6 +56,13 @@ if [[ $1 == "local" ]]; then
     cmd="nohup ${cmd}"
     eval $cmd >> $OUTDIR/run.log 2>&1 &
     echo "logpath: $OUTDIR/run.log"
+elif [[ $1 == "dryrun" ]]; then
+    echo "[INFO: dryrun, add --max_steps 25 to cli"
+    cmd="${cmd} --max_steps 25"
+    echo "${cmd}" > $OUTDIR/dryrun.log
+    echo "### End of CMD ---" >> $OUTDIR/dryrun.log
+    eval $cmd >> $OUTDIR/dryrun.log 2>&1 &
+    echo "logpath: $OUTDIR/dryrun.log"
 else
     source $CONDAROOT/miniconda3/etc/profile.d/conda.sh
     conda activate ${CONDAENV}
